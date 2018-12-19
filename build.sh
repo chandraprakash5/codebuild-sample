9231d785e86cd2d835f05ff08c97435fdb2dfe59
chandraprakash5 <chandraprakash54@hotmail.com>
2018-12-19 16:33:19 +0530

    Adding codebuild buildspec.yml

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cd $CODEBUILD_SRC_DIR
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
@@ -12,8 +13,8 @@ if [ $FOLDER1_COMMIT = $LATEST_COMMIT ] || [ $FOLDER2_COMMIT = $LATEST_COMMIT ];
     then
         echo "files in first_app has changed"
         docker build -t codebuild_first_app:$LATEST_COMMIT -f first_app/Dockerfile .
-#        docker tag codebuild_first_app:$LATEST_COMMIT $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
-#        docker push $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
+        docker tag codebuild_first_app:$LATEST_COMMIT $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
+        docker push $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
         git log -1 > commit_info.txt
 else
      echo "no folders of relevance has changed"
