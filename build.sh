a5f9e88f3612a366e6ca7876e6566b823d2bdbf7
chandraprakash5 <chandraprakash54@hotmail.com>
2018-12-19 14:58:45 +0530

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff first_app)
+FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff base_app)
+
+if [ $FOLDER1_COMMIT = $LATEST_COMMIT ] || [ $FOLDER2_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "files in first_app has changed"
+        docker build -t codebuild_first_app:$LATEST_COMMIT -f first_app/Dockerfile .
+#        docker tag codebuild_first_app:$LATEST_COMMIT $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
+#        docker push $DOCKER_ID_USER/codebuild_first_app:$LATEST_COMMIT
+        git log -1 > commit_info.txt
+else
+     echo "no folders of relevance has changed"
+     exit 0;
+fi
