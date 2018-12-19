a11a0dea25325bda72e599cbc0c375299f0dcdf7
chandraprakash5 <chandraprakash54@hotmail.com>
2018-12-19 14:36:13 +0530

    Added 3 microservices

---
+++ second_app/second_app.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.append('../base_app')
+
+from base_app import base_func
+ 
+print(base_func() + 'second_app' + str(1))
---
+++ third_app/third_app.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.append('../base_app')
+
+from base_app import base_func
+ 
+print(base_func() + 'third_app' + str(1))
