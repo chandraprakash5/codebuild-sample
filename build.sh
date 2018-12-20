1d64a52b419b27446735743098f56d0aed7cadb3
chandraprakash5 <chandraprakash54@hotmail.com>
2018-12-20 12:55:24 +0530

    Logging into dockerhub

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+docker login --username=$DOCKER_ID_USER --password="cP>140292"
 LATEST_COMMIT=$(git rev-parse HEAD)
 echo "files in first_app has changed"
 docker build -t codebuild_first_app:$LATEST_COMMIT -f first_app/Dockerfile .
